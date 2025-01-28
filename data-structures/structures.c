97b48cd49c178cc933d2fa2fb1df60b9b8e36c64
Paulo Marques <paulomarques.rs1@gmail.com>
2025-01-28 15:19:13 -0300

    first commit

---
+++ data-structures/structures.c
@@ -0,0 +1,27 @@
+typedef struct {
+    float weight;
+    float height;
+    int age;
+} WeightHeightAge;
+
+struct Car
+{
+    int id;
+    char model;
+    int year;
+};
+
+int structs() {
+    WeightHeightAge weightHeightAge;
+    weightHeightAge.weight = 70.5;
+    weightHeightAge.height = 1.75;
+
+    struct Car car;
+    car.id = 1;
+    car.model = 'A';
+    car.year = 2020;
+
+    printf("Weight: %f, Height: %f\n, Age: %i", weightHeightAge.weight, weightHeightAge.height, weightHeightAge.age);
+
+    return 0;
+}
